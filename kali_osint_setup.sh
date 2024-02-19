75691b2a81de8c16c0bb192edc9b1de12282aad2
Zombi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-02-19 08:27:46 -0800

    Update kali_osint_setup.sh

--- kali_osint_setup.sh
+++ kali_osint_setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-apt install tor ufw gospider hakrawler gobuster ruby-dev ffuf seclists npm youtubedl-gui filezilla libreoffice -y
+apt install tor ufw gospider hakrawler gobuster ruby-dev ffuf seclists youtubedl-gui filezilla libreoffice -y
 apt install httrack webhttrack sherlock eyewitness sublist3r photon theharvester recon-ng python3-venv jq pipx kazam golang mediainfo-gui -y
 apt install marble qgis finalrecon ugrep bloodhound bing-ip2hosts realtek-rtl88xxau-dkms mat2 gallery-dl libimage-exiftool-perl stegosuite exifprobe ruby-bundler mpg123 thunderbird sq python3-lxml libre-dev exiflooter -y
 gem install mechanize
 gem install colorize
-npm install -g ftp-spider
-npm i -g tiktok-scraper
+
 systemctl enable ufw
 ufw enable
-ufw allow ssh
-ufw allow 4444
 systemctl start postgresql
 systemctl enable postgresql
 systemctl start ssh

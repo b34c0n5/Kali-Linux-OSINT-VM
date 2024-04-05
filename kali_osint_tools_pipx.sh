b18fca0de1d1e9d5f71019ce4b0f4331a42a1b35
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-04-05 11:26:12 -0700

    Update kali_osint_tools_pipx.sh

--- kali_osint_tools_pipx.sh
+++ kali_osint_tools_pipx.sh
@@ -97,6 +97,9 @@ python3 -m venv wmn-pythonEnv
 source wmn-pythonEnv/bin/activate
 pip install -r requirements.txt
 deactivate
+wget https://github.com/tomnomnom/gron/releases/download/v0.7.1/gron-linux-amd64-0.7.1.tgz
+tar xzf gron-linux-amd64-0.7.1.tgz
+rm gron-linux-amd64-0.7.1.tgz
 
 cd ~/programs/
 git clone https://github.com/hatlord/Spiderpig.git

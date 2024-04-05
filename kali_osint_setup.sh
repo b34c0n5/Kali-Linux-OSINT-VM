e71741668c4f1fd5da92da45ec5c2fbae4d9fc2b
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-04-05 11:45:30 -0700

    Update kali_osint_setup.sh

--- kali_osint_setup.sh
+++ kali_osint_setup.sh
@@ -3,8 +3,8 @@
 apt install tor ufw gospider hakrawler gobuster ruby-dev ffuf seclists youtubedl-gui filezilla libreoffice -y
 apt install httrack webhttrack sherlock eyewitness sublist3r photon theharvester recon-ng python3-venv jq pipx -y
 apt install snapd golang mediainfo-gui kali-tools-crypto-stego -y
-apt install marble qgis finalrecon ugrep bloodhound bing-ip2hosts realtek-rtl88xxau-dkms 
-apt install mat2 gallery-dl libimage-exiftool-perl stegosuite exifprobe ruby-bundler mpg123 
+apt install marble qgis finalrecon ugrep bloodhound bing-ip2hosts realtek-rtl88xxau-dkms -y
+apt install mat2 gallery-dl libimage-exiftool-perl stegosuite exifprobe ruby-bundler mpg123 -y
 apt install thunderbird sq python3-lxml libre-dev exiflooter -y
 gem install mechanize
 gem install colorize

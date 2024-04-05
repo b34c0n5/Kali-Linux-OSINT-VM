ce63e394ab3846848f5cd899f8437d121ef0ce94
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-04-05 09:22:49 -0700

    Update kali_osint_setup.sh

--- kali_osint_setup.sh
+++ kali_osint_setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 apt install tor ufw gospider hakrawler gobuster ruby-dev ffuf seclists youtubedl-gui filezilla libreoffice -y
-apt install httrack webhttrack sherlock eyewitness sublist3r photon theharvester recon-ng python3-venv jq pipx kazam golang mediainfo-gui -y
-apt install marble qgis finalrecon ugrep bloodhound bing-ip2hosts realtek-rtl88xxau-dkms mat2 gallery-dl libimage-exiftool-perl stegosuite exifprobe ruby-bundler mpg123 thunderbird sq python3-lxml libre-dev exiflooter -y
+apt install httrack webhttrack sherlock eyewitness sublist3r photon theharvester recon-ng python3-venv jq pipx -y
+apt install snapd golang mediainfo-gui -y
+apt install marble qgis finalrecon ugrep bloodhound bing-ip2hosts realtek-rtl88xxau-dkms 
+apt install mat2 gallery-dl libimage-exiftool-perl stegosuite exifprobe ruby-bundler mpg123 
+apt install thunderbird sq python3-lxml libre-dev exiflooter -y
 gem install mechanize
 gem install colorize
-
+snap install youtube-dl-pro
 systemctl enable ufw
 ufw enable
 systemctl start postgresql

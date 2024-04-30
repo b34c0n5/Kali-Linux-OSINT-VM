920b891d542b6e225edfb1d6dd35bcfe03094ff4
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-04-30 13:22:11 -0700

    Update kali_osint_tools_pipx.sh

--- kali_osint_tools_pipx.sh
+++ kali_osint_tools_pipx.sh
@@ -122,7 +122,6 @@ git clone https://github.com/lolwaleet/ReverseIP.git
 git clone https://github.com/Raikia/UhOh365.git
 git clone https://github.com/HACK3RY2J/Anon-SMS.git
 git clone https://github.com/MohammedAlsubhi/instashell-master.git
-git clone https://github.com/pvanfas/socialphish.git
 git clone https://github.com/4n4nk3/Wordlister.git
 git clone https://github.com/netlas-io/netlas-scripts.git
 git clone https://github.com/TermuxHackz/X-osint

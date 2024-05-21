a77da12c92b52e3b063e21b839ea6c482bfc00c7
Z0mbi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-05-21 09:36:13 -0700

    Update snoint.sh

--- snoint.sh
+++ snoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt update
 apt install curl sq -y
 curl -sSf https://apt.vulns.xyz/kpcyrd.pgp | sq keyring filter -B --handle 64B13F7117D6E07D661BBCE0FE763A64F5E54FD6 | sudo tee /etc/apt/trusted.gpg.d/apt-vulns-xyz.gpg > /dev/null
 echo deb http://apt.vulns.xyz stable main | sudo tee /etc/apt/sources.list.d/apt-vulns-xyz.list

b2517e00c64594a996f564c9f0c2d3ca309a3aec
Zombi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-06-19 11:40:57 -0700

    Update kali_osint_setup.sh

--- kali_osint_setup.sh
+++ kali_osint_setup.sh
@@ -6,7 +6,7 @@ apt install httrack webhttrack sherlock eyewitness sublist3r photon recon-ng pyt
 apt install snapd mediainfo-gui kali-tools-crypto-stego neovim terminator cargo asciinema -y
 apt install marble qgis finalrecon ugrep bloodhound bing-ip2hosts realtek-rtl88xxau-dkms -y
 apt install mat2 gallery-dl libimage-exiftool-perl stegosuite exifprobe ruby-bundler mpg123 -y
-apt install thunderbird sq python3-lxml libre-dev exiflooter flowblade -y
+apt install thunderbird sq python3-lxml libre-dev exiflooter flowblade dumpsterdiver -y
 apt install npm -y
 apt purge -y spiderfoot amass theharvester
 apt autoremove -y

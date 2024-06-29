d209bb3f713f7db2daaf34ba2b72edb7a17f4477
Zombi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-06-28 19:33:16 -0700

    Update kali_osint_setup.sh

--- kali_osint_setup.sh
+++ kali_osint_setup.sh
@@ -3,7 +3,7 @@
 apt install tor ufw gospider hakrawler gobuster ruby-dev ffuf seclists youtubedl-gui filezilla libreoffice -y
 apt install httrack webhttrack sherlock eyewitness sublist3r photon recon-ng python3-venv python3-pip jq pipx -y
 apt install snapd mediainfo-gui kali-tools-crypto-stego neovim cargo asciinema -y
-apt install marble qgis finalrecon ugrep bloodhound bing-ip2hosts golang -y
+apt install marble qgis finalrecon ugrep bloodhound bing-ip2hosts golang libxcb-cursor0 -y
 apt install mat2 gallery-dl libimage-exiftool-perl stegosuite exifprobe ruby-bundler mpg123 -y
 apt install thunderbird sq python3-lxml libre-dev exiflooter flowblade dumpsterdiver -y
 apt install npm -y

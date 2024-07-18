e2ae045cb0768b04754196284b600e137edcef9d
Zombi3 <159464494+midnit3Z0mbi3@users.noreply.github.com>
2024-07-18 07:28:39 -0700

    Update kali_osint_tools_pipx.sh

--- kali_osint_tools_pipx.sh
+++ kali_osint_tools_pipx.sh
@@ -205,5 +205,6 @@ cd
 mkdir ~/.config/amass
 cd ~/.config/amass
 git clone https://github.com/proabiral/Fresh-Resolvers.git
-cd
 
+cd
+curl -o ronin-install.sh https://raw.githubusercontent.com/ronin-rb/scripts/main/ronin-install.sh && bash ronin-install.sh
